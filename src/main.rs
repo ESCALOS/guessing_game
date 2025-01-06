a3c3de46f1c7a8ced91d60120cffcb6760f94f6a
Nanoka <stornblood6969@gmail.com>
2025-01-06 12:04:48 -0500

    Finished game

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,31 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    
-    //Validar si es un número
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        
+        //Validar si es un número
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    //Verificar si elnúmero es igual o si está por debajo o por encima
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+        
+        //Verificar si elnúmero es igual o si está por debajo o por encima
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
-
-    println!("You guessed: {}", guess);
 }
